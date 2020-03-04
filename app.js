fe485291717598fc415b178ba5fba2b18e624b96
Omar <omar@omarsalempro.com>
2020-03-04 15:59:04 -0700

    Add registration page and bootstrap nav bar

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
+const mustacheExpress = require('mustache-express');
 const PORT = process.env.PORT || 3000;
 
+// Configure the view engine
+app.engine('mustache', mustacheExpress());
+app.set('views', './views');
+app.set('view engine', 'mustache');
+
+app.get('/register', (req, res) => {
+  res.render('register');
+});
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
