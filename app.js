1bd20f85b97b26489af2658560a50adc6d1ac17f
Omar <omar@omarsalempro.com>
2020-03-17 14:45:13 -0600

    Add bcrypt encryption to register user

--- app.js
+++ app.js
@@ -2,23 +2,48 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mustacheExpress = require('mustache-express');
+const pgp = require('pg-promise')();
+const bcrypt = require('bcrypt');
+
 const PORT = process.env.PORT || 3000;
+const CONNECTION_STRING = 'postgres://localhost:5433/newsdb';
+const SALT_ROUNDS = 10;
 
 // Configure the view engine
 app.engine('mustache', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mustache');
+
 app.use(bodyParser.urlencoded( {extended: false}) );
+
+const db = pgp(CONNECTION_STRING);
 
 app.post('/register', (req,res) => {
 
   let username = req.body.username;
   let password = req.body.password;
-
-  console.log(username);
-  console.log(password);
-
-  res.send('REGISTER');
+  
+  db.oneOrNone('SELECT userid FROM users WHERE username = $1', [username])
+  .then ((user) => {
+    if(user) {
+      res.render('register', {message: "User name already exists"})
+    } else {
+      // INSERT user name into the db
+      // db.none('INSERT INTO users(username, password) VALUES($1, $2)', [username, password])
+      // .then(() => {
+      //   res.send('SUCCESS')
+      // })
+      bcrypt .hash(password, SALT_ROUNDS, function(error, hash) {
+        if (error == null) {
+          db.none('INSERT INTO users(username, password) VALUES($1, $2)',[username, hash])
+          .then(() => {
+            res.send('SUCCESS')
+          })
+        }
+      } )
+    }
+  })
+ 
 
 });
 
@@ -29,3 +54,4 @@ app.get('/register', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
+
